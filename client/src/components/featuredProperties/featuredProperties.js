d2346b95ea31053a217adaff8d88d4681dc9c9f0
AJITH P. KUMAR <77005115+ajithpkumar18@users.noreply.github.com>
2024-12-04 15:14:07 +0530

    Update featuredProperties.js
    added the vercel link to backend

--- client/src/components/featuredProperties/featuredProperties.js
+++ client/src/components/featuredProperties/featuredProperties.js
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredproperties.css";
 
 function FeaturedProperties() {
-  const { data, loading, error } = useFetch("http://localhost:8800/api/hotels?featured=true&limit=4");
+  const { data, loading, error } = useFetch("https://luxury-inn.vercel.app/api/hotels?featured=true&limit=4");
   console.log(data);
 
   return (
